7b29a52bcd8b49dda7fbadcbefc9ff4754daf169
李正林 <614544959@qq.com>
2020-12-23 18:27:01 +0800

    add sdk

---
+++ ADSafeNSObjectFramework.framework/Headers/ADSafeNSObjectFramework.h
@@ -0,0 +1,30 @@
+//
+//  ADSafeNSObjectFramework.h
+//  ADSafeNSObjectFramework
+//
+//  Created by Mac on 2020/12/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ADSafeNSObjectFramework.
+FOUNDATION_EXPORT double ADSafeNSObjectFrameworkVersionNumber;
+
+//! Project version string for ADSafeNSObjectFramework.
+FOUNDATION_EXPORT const unsigned char ADSafeNSObjectFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ADSafeNSObjectFramework/PublicHeader.h>
+
+// 字典
+#import <ADSafeNSObjectFramework/NSDictionary+ADNilSafe.h>
+#import <ADSafeNSObjectFramework/NSDictionary+ADSafe.h>
+#import <ADSafeNSObjectFramework/NSMutableDictionary+ADSafe.h>
+
+// 数组
+#import <ADSafeNSObjectFramework/NSArray+ADSave.h>
+#import <ADSafeNSObjectFramework/NSArray+ADSafe.h>
+#import <ADSafeNSObjectFramework/NSMutableArray+ADSafe.h>
+
+// 字符串
+#import <ADSafeNSObjectFramework/NSMutableString+ADSafe.h>
+
